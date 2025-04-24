e5f8e4b70272b24476adcee84850d157fa127dc7
arthwr <artyr007@gmail.com>
2025-04-24 14:14:54 +0500

    feat(player): add player class and draw it on screen

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from player import Player
 from constants import *
 
 
@@ -7,7 +8,10 @@ def main():
 
     pygame.init()
 
+    dt = 0
+    game_clock = pygame.time.Clock()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     running = True
 
     while running:
@@ -16,8 +20,10 @@ def main():
                 running = False
 
         screen.fill("black")
+        player.draw(screen)
         
         pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
